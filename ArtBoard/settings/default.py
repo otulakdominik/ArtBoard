be1ce38c734154764a0482fd569dff47b7da59bc
Dawid <Dawid_pacanowski@wp.pl>
2020-06-01 18:20:25 +0200

    templates

--- ArtBoard/settings/default.py
+++ ArtBoard/settings/default.py
@@ -128,7 +128,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR + "/Templates", 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR + "/Templates", 'static')
+]
 
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
